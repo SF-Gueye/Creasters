57dd371a6e223bce82958876bad5b88526e5f09b
SF-Gueye <ultra-necrozma@hotmail.com>
2025-11-10 09:26:19 +0100

    Ajout du Projet sur Git

---
+++ src/main/kotlin/org/ldv/creasters/CreastersApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.creasters
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreastersApplication
+
+fun main(args: Array<String>) {
+    runApplication<CreastersApplication>(*args)
+}
---
+++ src/test/kotlin/org/ldv/creasters/CreastersApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ldv.creasters
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreastersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
