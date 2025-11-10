18393391ee2e44205fbb0715705b2306fb2938d7
SF-Gueye <ultra-necrozma@hotmail.com>
2025-11-10 12:02:39 +0100

    Ajout du Projet sur Git

---
+++ src/main/kotlin/org/ldv/creasters/controller/MainController.kt
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class MainController (){
+
+    /**
+     * Méthode permettant d'afficher la page d'accueil de l'application.
+     * @return le chemin vers le template a partir du dossier ressources/templates (on ne marque pas le .html)
+     */
+    @GetMapping("/Creasters")
+    fun home():String{
+        return "index"
+    }
+}
